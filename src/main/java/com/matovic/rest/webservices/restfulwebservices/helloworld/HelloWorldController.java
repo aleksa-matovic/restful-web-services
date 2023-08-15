0736edaa1405f294f84e7b0befaacddbdb43e10b
Aleksa Matovic <lexamatovic@gmail.com>
2023-08-15 17:24:19 +0200

    Hello world rest example

---
+++ src/main/java/com/matovic/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.matovic.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
